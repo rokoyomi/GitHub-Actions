313293bd474bd96835aa18617e6a1dc8d2eb906a
rokoyomi <rokoyom@gmail.comi>
2025-08-06 03:06:33 +0500

    new commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 
 int main(int argc, const char* argv[]) {
-
     if (argc != 2) {
         return -1;
     }
-
-    int n = atoi(argv[1]);
-    if (n == 1) {
-        return 0;
-    } else if (n == 2) {
-        return 0;
-    }
-
     return 0;
 }
