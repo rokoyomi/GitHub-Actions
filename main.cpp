0d604baf7ee264bc17084d985b8570d64e3442a0
rokoyomi <rokoyom@gmail.comi>
2025-08-06 02:50:41 +0500

    add action and base file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+
+    if (argc != 2) {
+        return -1;
+    }
+
+    int n = atoi(argv[1]);
+    if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 0;
+    }
+
+    return 0;
+}
